96bbef1b0791226b1a419c2e6db47b2ddcd27ee8
amalmusouka <amalsudhikumar20@gmail.com>
2026-03-03 12:09:01 +0100

    feat: final solution for base_conversion

--- base_conversion/base_conversion.pl
+++ base_conversion/base_conversion.pl
@@ -4,9 +4,10 @@ len([], 0).
 len([_ | L], N) :-
     N #> 0, N1 #= N - 1, len(L, N1).
 
-digits([X], B, N) :- B #> 0, B #=< 10, X #>= 0, X #< B, N #= X. 
+digits([X], B, N) :- B in 2..10, X #>= 0, X #< B, N #= X. 
 
 digits(L, B, N) :-
-    B #> 0, B #=< 10, reverse(L, RL), len(RL, C), append(XL, [X], RL), reverse(XL, RXL), digits(RXL, B, NN), N #= X * (B ^ (C - 1)) + NN.
+    B in 2..10, reverse(L, RL), len(RL, C), append(XL, [X], RL), X #>= 0, X #< B, reverse(XL, RXL), digits(RXL, B, NN), N #= X * (B ^ (C - 1)) + NN.
 
-% digits([1, 2, 3], 5, N).
+convert(L1, B1, L2, B2) :-
+    digits(L1, B1, N1), digits(L2, B2, N2), N2 #= N1.
